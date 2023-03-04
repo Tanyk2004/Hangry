253a436c9de24c7f19cd006bc404b92422711436
jellybean-7 <git@github.com>
2023-03-03 23:36:36 -0600

    trying textField

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
 
 
 function TextInput() {
+  const [value, setValue] = React.useState('');
+
   return (
     <div>
-      
+      <TextField id="standard-basic" 
+      label="Standard" 
+      variant="standard" />
     </div>
-  )
+  );
 }
 
 export default TextInput
