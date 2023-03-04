d37e7ceb676fe9e005ffcf802b0fc1760a9a62db
Tanay Garg <tanay.garg@utexas.edu>
2023-03-03 23:24:03 -0600

    set up component development

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import MainPage from './MainPage'
 function App() {
   return (
     <div>
+      <MainPage />
       <button>Click me</button>
     </div>
   )
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Card from '../components/Card'
+import TextInput from '../components/TextInput'
 function MainPage() {
   return (
     <div>
-      
+      <Card></Card>
+      <TextInput></TextInput>
     </div>
   )
 }
