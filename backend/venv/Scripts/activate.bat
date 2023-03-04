0d2c28333102eb0208956eeb8cd6890e94147bf8
Tanay Garg <tanay.garg@utexas.edu>
2023-03-03 21:10:39 -0600

    reset the virtual environment

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\coding_projects\hackathon-spring2023\Hangry\venv
+set VIRTUAL_ENV=C:\coding_projects\hackathon-spring2023\Hangry\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
