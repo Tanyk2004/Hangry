9d280afa2c1eef9815b356d48c4733b77120bc9a
Tanay Garg <tanay.garg@utexas.edu>
2023-03-03 23:37:01 -0600

    changed md file with libraries

---
+++ frontend/src/components/CustomCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
+import { Box } from '@mui/material'
+
+// this is our customzied card component
+function CustomCard() {
+  return (
+    <div>
+      <Card></Card>
+    </div>
+  )
+}
+
+export default Card
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../components/Card'
+import Card from '../components/CustomCard'
 import TextInput from '../components/TextInput'
 function MainPage() {
   return (
